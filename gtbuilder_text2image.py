320f678322ae76156f5d6b708f0dfd9c12d79bd8
Jean-René Bastien <jrbastien@yahoo.com>
2021-11-05 16:30:24 -0400

    Now saved Ground thruth text in unix format on Windows, added a 10 pixel white border around the image and set the property of the image to 300 dpi.

--- gtbuilder_text2image.py
+++ gtbuilder_text2image.py
@@ -14,7 +14,9 @@ gtfile = open("base-ground-truth.txt")
 lines = gtfile.read().split('\n')
 for (n, line)  in enumerate(lines[:-1]):
     # write a file for each line
-    open("./model-ground-truth/model-ground-truth-line{}".format(n+1) + ".gt.txt", "w" ).write(line)
+    gtfile = open("./model-ground-truth/model-ground-truth-line{}".format(n+1) + ".gt.txt", "w" )
+    gtfile.write(line + "\n")
+    gtfile.close()
     # create the corresponding image with Text2Image.  See text2image --help to change options
     os.system("text2image --ptsize=14 --xsize=14400 --ysize=300 --exposure=-1 --fonts_dir=./fonts --strip_unrenderable_words --leading=32 --char_spacing=0.0 --exposure=0 --outputbase=\"" + "./model-ground-truth/model-ground-truth-line{}".format(n+1) + "\" --font=\"" + sys.argv[1] + "\" --text=\"" + "./model-ground-truth/model-ground-truth-line{}".format(n+1) + ".gt.txt" + "\" --resolution=300")
     # delete the box file that is only need for Tesseract 3.x training.
@@ -24,4 +26,6 @@ for (n, line)  in enumerate(lines[:-1]):
     invert_im = ImageOps.invert(image.convert('RGB')) #convert to RGB
     x1,y1,x2,y2=invert_im.getbbox() #invert image as crop works on black pixels
     im1 = image.crop((x1, y1, x2, y2)) #crop the orinal image as per coordinates found on inverted image
-    im1.save("./model-ground-truth/model-ground-truth-line{}".format(n+1) + ".tif") 
+    # Add a white border of 10 pixels around the image
+    new_img = ImageOps.expand(im1, border=(10,10,10,10), fill="white")
+    new_img.save("./model-ground-truth/model-ground-truth-line{}".format(n+1) + ".tif", dpi=(300,300)) 
